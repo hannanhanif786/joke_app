3db4d58947debc306b25537c5da97e0aff25c9c8
hannanhanif786 <muhammadhannanhanif1122@gmail.com>
2024-06-24 14:31:03 +0500

    jokes template added

---
+++ user/views/joke_view.py
@@ -0,0 +1,19 @@
+import requests
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from user.models import Joke
+
+
+@login_required(login_url="login")
+def joke_view(request):
+    jokes = Joke.objects.filter(user=request.user)
+    return render(request, 'joke.html', {'jokes': jokes})
+
+
+@login_required(login_url="login")
+def create_joke(request):
+    response = requests.get('https://official-joke-api.appspot.com/random_joke')
+    if response.status_code == 200:
+        joke = response.json()
+        Joke.objects.create(user=request.user, setup=joke['setup'], punctuation=joke['punchline'])
+    return redirect('/')
